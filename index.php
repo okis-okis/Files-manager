63319662b4ef8a724a49650c034cdcd74bd52120
OKIS <67818930+okis-okis@users.noreply.github.com>
2022-06-10 12:45:12 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+$path = $argv[1];
+$currentStatus = [];
+readPath($path, $currentStatus);
+
+$currentTime = filemtime($path);
+while(true){
+    clearCache($path);
+    checkPath($path);
+    sleep(1);
+}
+
+function checkPath($path)
+{
+    global $currentStatus;
+    $newStatus = [];
+    readPath($path, $newStatus);
+    foreach($currentStatus as $file=>$time){
+        if(!isset($newStatus[$file])){
+            echo "File ".$file." was deleted...".PHP_EOL;
+        }
+        else if($newStatus[$file] !== $time){
+            echo "File ".$file." was modified".PHP_EOL;
+        }
+    }
+
+    foreach($newStatus as $file => $time){
+        if(!isset($currentStatus[$file])){
+            echo "File ".$file." was added...".PHP_EOL;
+        }
+    }
+
+    $currentStatus = $newStatus;
+}
+
+function clearCache($path)
+{
+    clearstatcache(false, $path);
+}
+
+function readPath($path, &$filesMap)
+{
+    $filesMap[$path] = filemtime($path);
+    if(is_dir($path)){
+        $files = scandir($path);
+        foreach($files as $file){
+            if($file == '.' || $file === '..'){
+                continue;
+            }
+            $fileName = $path .'/'.$file;
+            $filesMap[$fileName] = filemtime($fileName);
+            if(is_dir($fileName)){
+                readPath($fileName, $filesMap);
+            }
+        }
+    }
+}
